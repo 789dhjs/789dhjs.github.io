c947384417504f6fe9a7186fcf250c949031566e
789dhjs <949616534@qq.com>
2024-12-15 23:37:00 +0800

    Site updated: 2024-12-15 23:37:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/05/我的第一篇博客/","2024/12/14/JAVA/","2024/12/15/操作系统/","2024/12/05/测试/"];function toRandomPost(){
+var posts=["2024/12/14/JAVA/","2024/12/05/我的第一篇博客/","2024/12/05/测试/","2024/12/15/操作系统/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
