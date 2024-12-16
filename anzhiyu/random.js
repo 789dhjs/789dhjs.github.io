eaa4b01e53faff222e8c6a67988a73d6ae0b5412
789dhjs <949616534@qq.com>
2024-12-17 00:13:48 +0800

    Site updated: 2024-12-17 00:13:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/14/JAVA/","2024/12/16/基础概念与常识/","2024/12/05/我的第一篇博客/","2024/12/15/操作系统/","2024/12/05/测试/"];function toRandomPost(){
+var posts=["2024/12/14/JAVA/","2024/12/16/基础概念与常识/","2024/12/05/我的第一篇博客/","2024/12/15/操作系统/","2024/12/05/测试/","2024/12/17/设计模式/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
